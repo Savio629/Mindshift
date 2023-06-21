15761cd11ecfaf2779e7fefd30f9cd8397b8debc
SHRUTI DALVI <shrutimdalvi14@gmail.com>
2023-06-21 13:34:33 +0530

    Second push

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,5 +1,38 @@
 
 import React, { useState } from "react";
+import './Assessment/Assessment.js';
+
+import outerbox from "./images/outerbox.png"
+import github from "./images/github.png"
+import linkedin from "./images/linkedin.png"
+import instagram from "./images/instagram.png"
+import twitter from "./images/twitter.png"
+import tagline from "./images/tagline.png"
+import greenwavecontainer from "./images/greenwavecontainer.png"
+import whitewavecontainer from "./images/whitewavecontainer.png"
+import keyfeatures from "./images/key-features.png"
+import importancetitle from "./images/importancetitle.png"
+import mentalhealthtitle from "./images/mentalhealthtitle.png"
+import positivethoughtstitle from "./images/positivethoughtstitle.png"
+import joinus from "./images/join-us.png"
+import bubblesmentalhealth from "./images/bubblesmentalhealth.png"
+import bubblespositivethought from "./images/bubblespositivethought.png"
+import bubblesimportance from "./images/bubblesimportance.png"
+import backlinesmentalhealth from "./images/backlinesmentalhealth.png"
+import backlinesimportance from "./images/backlinesimportance.png"
+import backlinepositive from "./images/backlinepositive.png"
+import greenfootercontainer from "./images/greenfootercontainer.png"
+import whitefootercontainer from "./images/whitefootercontainer.png"
+import footerline from "./images/footerline.png"
+import copyright from "./images/copyright.png"
+import games from "./images/games.png"
+import community from "./images/community.png"
+import blog from "./images/blog.png"
+import calendar from "./images/calendar.png"
+import mentalhealth from "./images/mentalhealth.gif"
+import positivethought from "./images/positivethought.gif"
+import meditation from "./images/meditation.gif"
+import importance from "./images/importance.gif"
 
 import './lpage.css';
 
@@ -8,213 +41,95 @@ import './lpage.css';
 const Landingpage = () => {
 
     return (
-      <div className="index">
-      <div className="div">
-        <div className="overlap-group">
-          <div className="rectangle" />
-          <div className="rectangle-2" />
-          <img
-            className="find-hope-healing"
-            alt="Find hope healing"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c5a79e61d5a73e6633ccc/img/find-hope-healing-and-strength-in-our-community-dedicated-to-imp.svg"
-          />
-          <div className="text-wrapper-8">Start Assessment</div>
-          <div className="ellipse" />
-          <div className="ellipse-2" />
-          <div className="ellipse-3" />
-          <div className="ellipse-4" />
-          <img
-            className="img"
-            alt="Rectangle"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/rectangle-8.svg"
-          />
-          <img
-            className="rectangle-3"
-            alt="Rectangle"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/rectangle-7.svg"
-          />
-         
-          <img
-            className="key-features"
-            alt="Key features"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/key-features.svg"
-          />
-          <img
-            className="what-is-mental"
-            alt="What is mental"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/what-is-mental-health-.svg"
-          />
-          <img
-            className="emphasize-the-value"
-            alt="Emphasize the value"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/emphasize-the-value-of-positive-thoughts.svg"
-          />
-          <img
-            className="importance-of-mental"
-            alt="Importance of mental"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/importance-of-mental-well-being.svg"
-          />
-          <img
-            className="ellipse-5"
-            alt="Ellipse"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/ellipse-17@2x.png"
-          />
-          <div className="text-wrapper-9">Mood Tracker</div>
-          <div className="text-wrapper-10">Games</div>
-          <div className="text-wrapper-11">Community Support</div>
-          <div className="text-wrapper-12">Blog</div>
-          <p className="mental-health-is">
-            Mental health is about being emotionally and socially healthy - the way we think, feel and develop
-            relationships - and not merely the absence of a mental health condition.Mental stress, also known as
-            psychological stress, is a type of stress that affects a person&#39;s mental and emotional well-being. It
-            can be caused by a wide variety of factors, including work-related pressures, financial difficulties, family
-            problems, relationship issues, and health concerns, among others.
-          </p>
-          <p className="mental-stress-is">
-            {" "}
-            Mental stress is also often viewed as a response to external circumstances or events that cause us to feel
-            anxious, overwhelmed, or worried. Mental stress is not just the result of external factors, but is also
-            shaped by our internal responses and interpretations. By learning to recognize and manage our thoughts and
-            beliefs, we can reduce the impact of mental stress on our well-being and cultivate greater happiness and
-            resilience in our lives.
-          </p>
-          <p className="it-is-also-important">
-            It is also important to address any underlying issues that may be contributing to mental health challenges.
-            This may include addressing past trauma, improving communication skills, or developing better coping
-            mechanisms.
-            <br />
-            Ultimately, the key to curing mental health is to approach it holistically and to develop a personalized
-            treatment plan that takes into account each individual&#39;s unique needs and circumstances. With the right
-            support, resources, and strategies, it is possible to manage mental health challenges and live a fulfilling
-            and meaningful life.
-          </p>
-          <img
-            className="about-bubble"
-            alt="About bubble"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/about-s4-bubble-1.png"
-          />
-          <img
-            className="about-s-bubble"
-            alt="About bubble"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/about-s4-bubble-3.png"
-          />
-          <img
-            className="about"
-            alt="About"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/about-s4-bubble2-1.png"
-          />
-          <img
-            className="about-lines"
-            alt="About lines"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/about-s4-lines-1.png"
-          />
-          <img
-            className="rectangle-4"
-            alt="Rectangle"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/rectangle-28.svg"
-          />
-          <img
-            className="rectangle-5"
-            alt="Rectangle"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/rectangle-27.svg"
-          />
-          <p className="element-copyrights-by">
-            <span className="span">2023 copyrights by </span>
-            <span className="text-wrapper-13">Mindshift . </span>
-            <span className="text-wrapper-14">All Rights Reserved.</span>
-          </p>
-          <img
-            className="line"
-            alt="Line"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/line-4.svg"
-          />
-          <div className="logo">
-            <div className="text-wrapper-15">MINDSHIFT</div>
-          </div>
-          <img
-            className="copyright-all-rights"
-            alt="Copyright all rights"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/copyright-all-rights-reserved@2x.png"
-          />
-      
+<div className="mindshift">
+<div className="overlap-group-wrapper"> 
+<div className="overlap-group">
+ <img className="outerbox" alt="Outerbox" src={outerbox} />
+ <a href='./games/assessment'>
+ <button className="startassessment-btn" > 
+   Start Assessment
+ </button>
+ </a>
+ <img className="tagline" alt="Tagline" src={tagline} />
+<div className="circle" />
+<div className="div" />
+<div className="circle-2" />
+<div className="circle-3" />
+ <img className="greenwavecontainer" alt="Greenwavecontainer" src={greenwavecontainer} /> 
+ <img className="whitewavecontainer" alt="Whitewavecontainer" src={whitewavecontainer} /> 
+ <img className="key-features" alt="Key features" src={keyfeatures} />
+ <img className="mental-health-title" alt="Mental health title" src={mentalhealthtitle} />
+ <img className="positive-thoughts" alt="Positive thoughts" src={positivethoughtstitle} />   
+ <img className="importance-title" alt="Importance title" src={importancetitle}/>
+<div className="text-wrapper-2" > Mood Tracker</div>
+<div className="text-wrapper-3">Games</div> 
+<div className="text-wrapper-4">Community Support</div>
+<div className="text-wrapper-5">Blog</div> 
+ <p className="mental-health">
+ Mental health is about being emotionally and socially healthy - the way we think, feel and develop relationships - and not merely the absence of a mental health condition. Mental stress, also known as psychological stress, is a type of stress that affects a person&#39;s mental and emotional well-being. It
+ can be caused by a wide variety of factors, including work-related pressures, financial difficulties, family problems, relationship issues, and health concerns, among others.
+ </p>
+ <p className="p">
+  {" "}
+  Mental stress is also often viewed as a response to external circumstances or events that cause us to feel
+  anxious, overwhelmed, or worried. Mental stress is not just the result of external factors, but is also
+  shaped by our internal responses and interpretations. By learning to recognize and manage our thoughts and
+  beliefs, we can reduce the impact of mental stress on our well-being and cultivate greater happiness and
+  resilience in our lives.
+  </p>
+  <p className="importance">
+  It is also important to address any underlying issues that may be contributing to mental health challenges.
+  This may include addressing past trauma, improving communication skills, or developing better coping
+  mechanisms. <br />
+  Ultimately, the key to curing mental health is to approach it holistically and to develop a personalized
+  treatment plan that takes into account each individual&#39;s unique needs and circumstances. With the right
+  support, resources, and strategies, it is possible to manage mental health challenges and live a fulfilling
+  and meaningful life.
+  </p>
+  <img className= "bubblesmentalhealth" alt="Bubblesmentalhealth" src={bubblesmentalhealth} />
+  <img className="img" alt="Img" src={bubblespositivethought}/>
+  <img className="bubblesimportance" alt="Bubblesimportance" src= {bubblesimportance} />
+  <img className="Img-2" alt= "Img" src={backlinesmentalhealth} />
+  <img className="backlinesimportance" alt="Backlinesimportance" src= {backlinesimportance} /> 
+  <img className="backlinepositive" alt="Backlinepositive" src={backlinepositive} />
+  <img className="greenfootercontainer" alt="Greenfootercontainer" src={greenfootercontainer} />
+  <img className="whitefootercontainer" alt="Whitefootercontainer" src={whitefootercontainer} />
 
-          <div className="text-wrapper-17"></div>
-          <div className="text-wrapper-18"></div>
-          <div className="rectangle-7" />
-          <img
-            className="git-hub"
-            alt="Git hub"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/github@2x.png"
-          />
-          <img
-            className="linked-in"
-            alt="Linked in"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/linkedin@2x.png"
-          />
-          <img
-            className="twitter"
-            alt="Twitter"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/twitter@2x.png"
-          />
-           <img
-            className="instagram"
-            alt="Instagram"
-            src="https://cdn-icons-png.flaticon.com/512/2175/2175198.png"
-          />
-          <img
-            className="join-us"
-            alt="Join us"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/join-us.svg"
-          />
-          <img
-            className="ellipse-7"
-            alt="Ellipse"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/ellipse-27@2x.png"
-          />
-          <img
-            className="ellipse-8"
-            alt="Ellipse"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/ellipse-28@2x.png"
-          />
-          <img
-            className="ellipse-9"
-            alt="Ellipse"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/ellipse-29@2x.png"
-          />
-          <img
-            className="rectangle-8"
-            alt="Rectangle"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c505ae61d5a73e6633cc6/img/rectangle.gif"
-          />
-          <img
-            className="https-lottiefiles"
-            alt="Https lottiefiles"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c58e4c41da9e14e013633/img/https---lottiefiles-com-125300-mental-health.gif"
-          />
-          <img
-            className="https-lottiefiles-2"
-            alt="Https lottiefiles"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c505ae61d5a73e6633cc6/img/https---lottiefiles-com-118691-mental-health-awareness.gif"
-          />
-          <img
-            className="https-lottiefiles-3"
-            alt="Https lottiefiles"
-            src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c5a79e61d5a73e6633ccc/img/https---lottiefiles-com-43792-yoga-se-hi-hoga.gif"
-          />
-        </div>
-        <div className="rectangle-9" />
-        <img
-          className="contact-globe"
-          alt="Contact globe"
-          src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/iteck---google-chrome-6-9-2023-3-53-30-pm-1.png"
-        />
-        <img
-          className="iteck-google-chrome"
-          alt="Iteck google chrome"
-          src="https://anima-uploads.s3.amazonaws.com/projects/648c4abd7a44d675082dc3c3/releases/648c4acf51322f22d1536d53/img/iteck---google-chrome-6-9-2023-3-53-30-pm-1.png"
-        />
-      </div>
-    </div> 
+ <p className="copyright">
+<span className="span">2023 copyrights by </span>
+<span className="text-wrapper-6">Mindshift.</span>
+<span className="text-wrapper-7">All Rights Reserved.</span>
+</p>
+<img className="footerline" alt="Footerline" src={footerline} />
+<div className="logo">
+<h1 className="h-1">MINDSHIFT</h1> 
+</div>
+<img className="copyright-2" alt="Copyright" src={copyright} />
+<a href="https://github.com" target="_blank">
+  <img className="git-hub" alt="GitHub" src={github} />
+</a>
+<a href="https://linkedin.com" target="_blank">
+  <img className="linked-in" alt="LinkedIn" src={linkedin} />
+</a>
+<a href="https://twitter.com" target="_blank">
+  <img className="twitter" alt="Twitter" src={twitter} />
+</a>
+<a href="https://instagram.com" target="_blank">
+  <img className="instagram" alt="Instagram" src={instagram} />
+</a>
+<img className="join-us" alt="Join us" src={joinus} />
+<img className="games" alt="Games" src={games} />
+<img className="community" alt="Community" src={community} />
+<img className="blog" alt="Blog" src={blog} />
+<img className="calendar" alt="Calendar" src={calendar}/>
+<img className="importance-2" alt="Importance" src={importance}/>
+<img className="mentalhealth" alt="Mentalhealth" src={mentalhealth} />
+<img className="positivethought" alt="Positivethought" src={positivethought} />
+<img className="meditation" alt="Meditation" src={meditation} />
+</div>
+</div>
+</div>
+
   )
 }
 
